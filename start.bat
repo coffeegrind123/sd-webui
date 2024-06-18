19c2658cb9746c44e403c9db0a6254676e9e9b3a
coffeegrind123 <103264516+coffeegrind123@users.noreply.github.com>
2024-06-18 05:18:54 +0300

    Update start.bat

--- start.bat
+++ start.bat
@@ -14,10 +14,6 @@ xcopy /s /e /y "..\data" ".\data\"
 
 REM https://github.com/AbdBarho/stable-diffusion-webui-docker/wiki/Setup
 docker compose --profile download up --build
-REM wait until its done, then:
-REM docker compose --profile [ui] up --build
-REM where [ui] is one of: invoke | auto | auto-cpu | comfy | comfy-cpu
-REM docker compose --profile auto up --build
 docker compose --profile auto up
 
 REM Close the command prompt after the script finishes
